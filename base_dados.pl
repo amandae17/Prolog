87a6bc91e301e3cb623833168e590b378f879801
amandae17 <amandaelima1@gmail.com>
2023-03-27 20:05:49 -0300

    Update

---
+++ base_dados.pl
@@ -0,0 +1,81 @@
+% Saida
+consulta([sim|_], '').
+% Validar
+opcao_massa(1).
+opcao_massa(2).
+opcao_massa(3).
+opcao_massa(4).
+opcao_recheio(5).
+opcao_recheio(6).
+opcao_recheio(7).
+opcao_queijo(8).
+opcao_queijo(9).
+opcao_queijo(10).
+opcao_queijo(11).
+opcao_complemento(12).
+opcao_complemento(13).
+opcao_complemento(14).
+opcao_complemento(15).
+opcao_complemento(16).
+opcao_complemento(17).
+% Listagem
+consulta([lista, massa|_],
+'
+1 - Tradicional
+2 - Integral
+3 - Especial
+4 - Fermantecao Natural').
+consulta([lista, recheio|_],
+'
+5 - Surpresa
+6 - Moda Casa
+7 - Vegetariana').
+consulta([lista, queijo|_],
+'
+8 - Mussarela
+9 - Cheddar
+10 - Parmesao
+11 - Brie').
+consulta([lista, complemento|_],
+'
+12 - Oregano
+13 - Tomate
+14 - Cebola
+15 - Azeitona
+16 - Brocolis
+17 - Nenhum').
+% Consulta das respostas
+consulta( [1|_],
+'Tradicional').
+consulta( [2|_],
+'Integral').
+consulta( [3|_],
+'Especial').
+consulta( [4|_],
+'Fermantecao Natural').
+consulta( [5|_],
+'Surpresa').
+consulta( [6|_],
+'Moda Casa').
+consulta( [7|_],
+'Vegetariana').
+consulta( [8|_],
+'Mussarela').
+consulta( [9|_],
+'Cheddar').
+consulta( [10|_],
+'Parmesao').
+consulta( [11|_],
+'Brie').
+consulta( [12|_],
+'Oregano').
+consulta( [13|_],
+'Tomate').
+consulta( [14|_],
+'Cebola').
+consulta( [15|_],
+'Azeitona').
+consulta( [16|_],
+'Brocolis').
+consulta( [17|_],
+'Nenhum').
---
+++ horas_complementares.pl
@@ -0,0 +1,57 @@
+%% CHATBOT - MONTE SUA PIZZA - PizzaVirtual
+:-include(base_dados).
+burger:-
+nl, format('Bem vindo a PizzaVirtual'), nl,
+format('Para comecar digite seu nome: '),
+read(Nome), nl,
+format('Bem vindo '), format(Nome), format(',
+favor inserir seu cpf: '),
+read(Cpf), nl, format(',
+favor inserir endereco: '),
+read(Endereco), nl,
+nl, format('------MONTE SUA PIZZA------'),
+nl,
+repeat,
+format('Selecione a massa desejado:'), nl,
+consulta([lista, massa|_], A1),
+format(A1),
+nl, read(Massa),
+opcao_massa(Massa),
+repeat,
+nl, format('Selecione o recheio desejado:'),
+nl,
+consulta([lista, recheio|_], A2),
+format(A2),
+nl, read(Recheio),
+opcao_recheio(Recheio),
+repeat,
+nl, format('Selecione o queijo desejado:'),
+nl,
+consulta([lista, queijo|_], A3),
+format(A3),
+nl, read(Queijo),
+opcao_queijo(Queijo),
+repeat,
+nl, format('Selecione o complemento desejado:'), nl,
+consulta([lista, complemento|_], A4),
+format(A4),
+nl, read(Complemento),
+opcao_complemento(Complemento),
+lanche(Massa, Recheio, Queijo, Complemento),
+nl, nl, format('Pedido finalizado!'),
+encerra_sessao('sim', Nome).
+consulta_base_dados(Entrada, Resposta) :-
+consulta(Entrada, Resposta), !.
+encerra_sessao('sim', Nome):-
+nl, nl, format('Obrigado, '), format(Nome),
+format(' dentro do prazo de 1 hora seu pedido estara chegando!!!'), nl, nl, !.
+lanche(Massa, Recheio, Queijo, Complemento) :-
+consulta([Massa|_], Mtxt),
+consulta([Recheio|_], Rtxt),
+consulta([Queijo|_], Qtxt),
+consulta([Complemento|_], Ctxt),
+nl, nl, format('Sua pizza:'),
+nl, format(Mtxt),
+nl, format(Rtxt),
+nl, format(Qtxt),
+nl, format(Ctxt).
